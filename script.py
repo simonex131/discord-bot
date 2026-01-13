2717147b0d06d94ae731debe3ea1852d5e0c9551
simonex131 <remgood.sikora@gmail.com>
2026-01-13 13:06:55 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -1,5 +1,21 @@
 import discord
 from discord.ext import commands
+from flask import Flask
+from threading import Thread
+
+# --- KEEP ALIVE ---
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 # --- INTENTS ---
 intents = discord.Intents.default()
